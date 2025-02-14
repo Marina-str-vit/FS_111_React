2acd592bd2e9cec184724bce1e8a85c5881275da
Marina Strukova <meribsv@gmail.com>
2025-02-14 14:23:54 +0200

    Update actions.js

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,8 +1,12 @@
 import { createAction } from '@reduxjs/toolkit';
+
+//ЦЕ МОЖЕ БУТИ В СТАРИХ КОДАХ
 // зберігаємо значення, щоб потім легше була перевірка
+
 // export const reset = { type: 'reset' };
 // export const increment = { type: 'INCREMENT' };
 // export const decrement = { type: 'DECREMENT' };
+
 // функц. для зміни значення step
 // export const changeStep = newStep => {
 //   return {
@@ -11,10 +15,11 @@ import { createAction } from '@reduxjs/toolkit';
 //   };
 // };
 
+// НОВА ВЕРСІЯ reduxjs/toolkit дозволяє все скоротити !!! Головне використовувати createAction
 export const reset = createAction('reset');
 export const increment = createAction('increment');
 export const decrement = createAction('decrement');
-export const changeStep = createAction('changeStep');
+export const changeStep = createAction('changeStep'); // тут також нічого зайвого не пишем
 
 // console.log(changeStep());
 // console.log(changeStep(10));
