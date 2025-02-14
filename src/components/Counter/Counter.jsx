aa52ce2d2955dfc46d002daba44ce9a22f64b43a
Marina Strukova <meribsv@gmail.com>
2025-02-14 13:52:01 +0200

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,29 +1,29 @@
 import { useDispatch, useSelector } from 'react-redux';
 import s from './Counter.module.css';
 import { increment, decrement, reset, changeStep } from '../../redux/counterSlice';
-
+//%% тут відбуваються дії клієнта-замовника,
 export const Counter = () => {
 //перше замовлення дати зі складу дані про counter та дані про step
 //useSelector має доступ до всього складу
 //                        весь склад   Склад.Зона_на_складі.Частина, що потрібна по запиту
   const counter = useSelector(state => state.counter.counter);
   const step = useSelector(state => state.counter.step);
-//адмін, хук_redux котрий керує всіми процесами на складі, вона ОДНА!! Диспечер!
+//адмін, хук_redux котрий керує всіми процесами на складі, він ОДИН!! Диспетчер!
   const dispatch = useDispatch();
-
+//%% замовник хоче збільшити значення, тоді ч/з dispatch йдемо у counter.js файл і дивимося, як це зробити
   const handlePlusClick = () => {
 //dispatch({type: 'INKREMENT'}) це початкове пояснення
     dispatch(increment());
   };
-
+//%% замовник хоче зменшити значення, тоді ч/з dispatch йдемо у counter.js файл і дивимося, як це зробити
   const handleMinusClick = () => {
     dispatch(decrement());
   };
-
+//%% замовник хоче видалити значення, робить все адмін dispatch ч/з робітника reducer
   const handleResetClick = () => {
     dispatch(reset());
   };
-
+//%% все ще лише бажання замовника, а робить все dispatch
   const handleChangeStep = e => {
     dispatch(changeStep(+e.target.value));
   };
