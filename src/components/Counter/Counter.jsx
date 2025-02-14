5049d97f5d9c9defe16fb9f1e45077598c46e401
Marina Strukova <meribsv@gmail.com>
2025-02-14 13:42:31 +0200

    Update Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,12 +3,16 @@ import s from './Counter.module.css';
 import { increment, decrement, reset, changeStep } from '../../redux/counterSlice';
 
 export const Counter = () => {
+//перше замовлення дати зі складу дані про counter та дані про step
+//useSelector має доступ до всього складу
+//                        весь склад   Склад.Зона_на_складі.Частина, що потрібна по запиту
   const counter = useSelector(state => state.counter.counter);
   const step = useSelector(state => state.counter.step);
-
+//адмін, хук_redux котрий керує всіми процесами на складі, вона ОДНА!! Диспечер!
   const dispatch = useDispatch();
 
   const handlePlusClick = () => {
+//dispatch({type: 'INKREMENT'}) це початкове пояснення
     dispatch(increment());
   };
 
